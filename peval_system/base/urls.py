d655bb4b763119d03bf45f6346477afa881f40f6
Gabriel Dealca <gudealca@up.edu.ph>
2022-05-24 09:35:58 +0800

    edit and delete functions for faculty

--- peval_system/base/urls.py
+++ peval_system/base/urls.py
@@ -13,6 +13,7 @@ app_name = 'base'
 
 urlpatterns =[
     path("profile", views.view_profile, name='view_profile'),
+    
     path("add_educ_att", views.add_educ_att, name='add_educ_att'),
     path("add_workexp", views.add_workexp, name='add_workexp'),
     path("add_pub", views.add_pub, name='add_pub'),
@@ -22,7 +23,26 @@ urlpatterns =[
     path("add_trainsem", views.add_trainsem, name='add_trainsem'),
     path("add_confwork", views.add_confwork, name='add_confwork'),
     path("add_extserv", views.add_extserv, name='add_extserv'),
-	
+
+    path("edit_educ_att/<int:pk>/", views.edit_educ_att, name='edit_educ_att'),
+    path("edit_workexp/<int:pk>/", views.edit_workexp, name='edit_workexp'),
+    path("edit_acc_events/<int:pk>/", views.edit_acc_events, name='edit_acc_events'),
+    path("edit_pub/<int:pk>/", views.edit_pub, name='edit_pub'),
+    path("edit_res_grants/<int:pk>/", views.edit_res_grants, name='edit_res_grants'),
+	path("edit_licexam/<int:pk>/", views.edit_licexam, name='edit_licexam'),
+    path("edit_trainsem/<int:pk>/", views.edit_trainsem, name='edit_trainsem'),
+    path("edit_confwork/<int:pk>/", views.edit_confwork, name='edit_confwork'),
+    path("edit_extserv/<int:pk>/", views.edit_extserv, name='edit_extserv'),
+    
+    path("delete_educ_att/<int:pk>/", views.delete_educ_att, name='delete_educ_att'),
+    path("delete_workexp/<int:pk>/", views.delete_workexp, name='delete_workexp'),
+    path("delete_acc_events/<int:pk>/", views.delete_acc_events, name='delete_acc_events'),
+    path("delete_pub/<int:pk>/", views.delete_pub, name='delete_pub'),
+    path("delete_res_grants/<int:pk>/", views.delete_res_grants, name='delete_res_grants'),
+    path("delete_licexam/<int:pk>/", views.delete_licexam, name='delete_licexam'),
+    path("delete_trainsem/<int:pk>/", views.delete_trainsem, name='delete_trainsem'),
+    path("delete_confwork/<int:pk>/", views.delete_confwork, name='delete_confwork'),
+    path("delete_extserv/<int:pk>/", views.delete_extserv, name='delete_extserv'),
 ]
 
 urlpatterns += static(settings.UPLOAD_URL, document_root=settings.UPLOAD_ROOT)
--- peval_system/base/views.py
+++ peval_system/base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.contrib import auth, messages
 from .models import *
 from .forms import *
 
@@ -57,16 +58,16 @@ def add_educ_att(request):
                 pass
             context = {}
             context['form'] = EducAttForm()
-            template = loader.get_template('add_educ_att.html')
+            template = loader.get_template('add_cred.html')
             return HttpResponse(template.render(context, request))
         else:
             print('not post')
             form = EducAttForm()
-            return render(request, 'add_educ_att.html', {'form': form})
+            return render(request, 'add_cred.html', {'form': form})
     else:
         print('not user')
         form = EducAttForm()
-        return render(request, 'add_educ_att.html', {'form': form})
+        return render(request, 'add_cred.html', {'form': form})
 
 def add_workexp(request):
     user = User.objects.get(id=request.user.id)
@@ -89,16 +90,16 @@ def add_workexp(request):
                 pass
             context = {}
             context['form'] = WorkExpForm()
-            template = loader.get_template('add_workexp.html')
+            template = loader.get_template('add_cred.html')
             return HttpResponse(template.render(context, request))
         else:
             print('not post')
             form = WorkExpForm()
-            return render(request, 'add_workexp.html', {'form': form})
+            return render(request, 'add_cred.html', {'form': form})
     else:
         print('not user')
         form = WorkExpForm()
-        return render(request, 'add_workexp.html', {'form': form})
+        return render(request, 'add_cred.html', {'form': form})
 
 def add_acc_events(request):
     user = User.objects.get(id=request.user.id)
@@ -121,16 +122,16 @@ def add_acc_events(request):
                 pass
             context = {}
             context['form'] = AccEventsForm()
-            template = loader.get_template('add_acc_events.html')
+            template = loader.get_template('add_cred.html')
             return HttpResponse(template.render(context, request))
         else:
             print('not post')
             form = AccEventsForm()
-            return render(request, 'add_acc_events.html', {'form': form})
+            return render(request, 'add_cred.html', {'form': form})
     else:
         print('not user')
         form = AccEventsForm()
-        return render(request, 'add_acc_events.html', {'form': form})
+        return render(request, 'add_cred.html', {'form': form})
 
 def add_pub(request):
     user = User.objects.get(id=request.user.id)
@@ -153,16 +154,16 @@ def add_pub(request):
                 pass
             context = {}
             context['form'] = PubForm()
-            template = loader.get_template('add_pub.html')
+            template = loader.get_template('add_cred.html')
             return HttpResponse(template.render(context, request))
         else:
             print('not post')
             form = PubForm()
-            return render(request, 'add_pub.html', {'form': form})
+            return render(request, 'add_cred.html', {'form': form})
     else:
         print('not user')
         form = PubForm()
-        return render(request, 'add_pub.html', {'form': form})
+        return render(request, 'add_cred.html', {'form': form})
 
 
 def add_res_grants(request):
@@ -186,16 +187,16 @@ def add_res_grants(request):
                 pass
             context = {}
             context['form'] = ResearchGrantsForm()
-            template = loader.get_template('add_res_grants.html')
+            template = loader.get_template('add_cred.html')
             return HttpResponse(template.render(context, request))
         else:
             print('not post')
             form = ResearchGrantsForm()
-            return render(request, 'add_res_grants.html', {'form': form})
+            return render(request, 'add_cred.html', {'form': form})
     else:
         print('not user')
         form = ResearchGrantsForm()
-        return render(request, 'add_res_grants.html', {'form': form})  
+        return render(request, 'add_cred.html', {'form': form})  
 
 def add_licexam(request):
     user = User.objects.get(id=request.user.id)
@@ -218,16 +219,16 @@ def add_licexam(request):
                 pass
             context = {}
             context['form'] = LicExamForm()
-            template = loader.get_template('add_licexam.html')
+            template = loader.get_template('add_cred.html')
             return HttpResponse(template.render(context, request))
         else:
             print('not post')
             form = LicExamForm()
-            return render(request, 'add_licexam.html', {'form': form})
+            return render(request, 'add_cred.html', {'form': form})
     else:
         print('not user')
         form = LicExamForm()
-        return render(request, 'add_licexam.html', {'form': form})  
+        return render(request, 'add_cred.html', {'form': form})  
 
 def add_trainsem(request):
     user = User.objects.get(id=request.user.id)
@@ -250,16 +251,16 @@ def add_trainsem(request):
                 pass
             context = {}
             context['form'] = TrainSemForm()
-            template = loader.get_template('add_trainsem.html')
+            template = loader.get_template('add_cred.html')
             return HttpResponse(template.render(context, request))
         else:
             print('not post')
             form = TrainSemForm()
-            return render(request, 'add_trainsem.html', {'form': form})
+            return render(request, 'add_cred.html', {'form': form})
     else:
         print('not user')
         form = TrainSemForm()
-        return render(request, 'add_trainsem.html', {'form': form})  
+        return render(request, 'add_cred.html', {'form': form})  
 
 def add_confwork(request):
     user = User.objects.get(id=request.user.id)
@@ -282,16 +283,16 @@ def add_confwork(request):
                 pass
             context = {}
             context['form'] = ConfWorkForm()
-            template = loader.get_template('add_confwork.html')
+            template = loader.get_template('add_cred.html')
             return HttpResponse(template.render(context, request))
         else:
             print('not post')
             form = ConfWorkForm()
-            return render(request, 'add_confwork.html', {'form': form})
+            return render(request, 'add_cred.html', {'form': form})
     else:
         print('not user')
         form = ConfWorkForm()
-        return render(request, 'add_confwork.html', {'form': form})  
+        return render(request, 'add_cred.html', {'form': form})  
 
 def add_extserv(request):
     user = User.objects.get(id=request.user.id)
@@ -314,14 +315,251 @@ def add_extserv(request):
                 pass
             context = {}
             context['form'] = ExtServForm()
-            template = loader.get_template('add_extserv.html')
+            template = loader.get_template('add_cred.html')
             return HttpResponse(template.render(context, request))
         else:
             print('not post')
             form = ExtServForm()
-            return render(request, 'add_extserv.html', {'form': form})
+            return render(request, 'add_cred.html', {'form': form})
     else:
         print('not user')
         form = ExtServForm()
-        return render(request, 'add_extserv.html', {'form': form})  
+        return render(request, 'add_cred.html', {'form': form})  
   
+def edit_educ_att(request, pk):
+    entry = get_object_or_404(EducationalAttainment, pk=pk)
+    
+    if request.method == "POST":
+        #print('post')
+        tempForm = EducAttForm(request.POST, request.FILES, instance=entry)
+        if tempForm.is_valid():
+            print('valid')       
+            instance = tempForm.save(commit=False)           
+            instance.save()
+            messages.success(request, "Changes saved successfully!")       
+            return redirect("/profile")                    
+        else:
+            messages.error(request, "Unsuccessful")
+    else:
+        print('not post?')
+        tempForm = EducAttForm(instance=entry)
+        context = {
+                'tempForm':tempForm,		
+                }
+        return render(request, 'edit_cred.html', context)
+
+def edit_workexp(request, pk):
+    entry = get_object_or_404(WorkExperience, pk=pk)
+    
+    if request.method == "POST":
+        #print('post')
+        tempForm = WorkExpForm(request.POST, request.FILES, instance=entry)
+        if tempForm.is_valid():
+            print('valid')       
+            instance = tempForm.save(commit=False)           
+            instance.save()
+            messages.success(request, "Changes saved successfully!")       
+            return redirect("/profile")                    
+        else:
+            messages.error(request, "Unsuccessful")
+    else:
+        print('not post?')
+        tempForm = WorkExpForm(instance=entry)
+        context = {
+                'tempForm':tempForm,		
+                }
+        return render(request, 'edit_cred.html', context)
+
+def edit_acc_events(request, pk):
+    entry = get_object_or_404(AccomplishmentsEvents, pk=pk)
+    
+    if request.method == "POST":
+        #print('post')
+        tempForm = AccEventsForm(request.POST, request.FILES, instance=entry)
+        if tempForm.is_valid():
+            print('valid')       
+            instance = tempForm.save(commit=False)           
+            instance.save()
+            messages.success(request, "Changes saved successfully!")       
+            return redirect("/profile")                    
+        else:
+            messages.error(request, "Unsuccessful")
+    else:
+        print('not post?')
+        tempForm = AccEventsForm(instance=entry)
+        context = {
+                'tempForm':tempForm,		
+                }
+        return render(request, 'edit_cred.html', context)
+
+def edit_pub(request, pk):
+    entry = get_object_or_404(Publications, pk=pk)
+    #co_author = get_object_or_404(User, pk=pk)
+    
+    if request.method == "POST":
+        #print('post')
+        tempForm = PubForm(request.POST, request.FILES, instance=entry)
+        if tempForm.is_valid():
+            print('valid')       
+            instance = tempForm.save(commit=False)    
+            #instance.co_author_DPSM.set() = co_author
+            instance.save()
+            messages.success(request, "Changes saved successfully!")       
+            return redirect("/profile")                    
+        else:
+            messages.error(request, "Unsuccessful")
+    else:
+        print('not post?')
+        tempForm = PubForm(instance=entry)
+        context = {
+                'tempForm':tempForm,		
+                }
+        return render(request, 'edit_cred.html', context)
+
+def edit_res_grants(request, pk):
+    entry = get_object_or_404(ResearchGrants, pk=pk)
+    
+    if request.method == "POST":
+        #print('post')
+        tempForm = ResearchGrantsForm(request.POST, request.FILES, instance=entry)
+        if tempForm.is_valid():
+            print('valid')       
+            instance = tempForm.save(commit=False)           
+            instance.save()
+            messages.success(request, "Changes saved successfully!")       
+            return redirect("/profile")                    
+        else:
+            messages.error(request, "Unsuccessful")
+    else:
+        print('not post?')
+        tempForm = ResearchGrantsForm(instance=entry)
+        context = {
+                'tempForm':tempForm,		
+                }
+        return render(request, 'edit_cred.html', context)
+
+def edit_licexam(request, pk):
+    entry = get_object_or_404(LicensureExam, pk=pk)
+    
+    if request.method == "POST":
+        #print('post')
+        tempForm = LicExamForm(request.POST, request.FILES, instance=entry)
+        if tempForm.is_valid():
+            print('valid')       
+            instance = tempForm.save(commit=False)           
+            instance.save()
+            messages.success(request, "Changes saved successfully!")       
+            return redirect("/profile")                    
+        else:
+            messages.error(request, "Unsuccessful")
+    else:
+        print('not post?')
+        tempForm = LicExamForm(instance=entry)
+        context = {
+                'tempForm':tempForm,		
+                }
+        return render(request, 'edit_cred.html', context)
+
+def edit_trainsem(request, pk):
+    entry = get_object_or_404(TrainingSeminars, pk=pk)
+    
+    if request.method == "POST":
+        #print('post')
+        tempForm = TrainSemForm(request.POST, request.FILES, instance=entry)
+        if tempForm.is_valid():
+            print('valid')       
+            instance = tempForm.save(commit=False)           
+            instance.save()
+            messages.success(request, "Changes saved successfully!")       
+            return redirect("/profile")                    
+        else:
+            messages.error(request, "Unsuccessful")
+    else:
+        print('not post?')
+        tempForm = TrainSemForm(instance=entry)
+        context = {
+                'tempForm':tempForm,		
+                }
+        return render(request, 'edit_cred.html', context)
+
+def edit_confwork(request, pk):
+    entry = get_object_or_404(ConferenceWorkshops, pk=pk)
+    
+    if request.method == "POST":
+        #print('post')
+        tempForm = ConfWorkForm(request.POST, request.FILES, instance=entry)
+        if tempForm.is_valid():
+            print('valid')       
+            instance = tempForm.save(commit=False)           
+            instance.save()
+            messages.success(request, "Changes saved successfully!")       
+            return redirect("/profile")                    
+        else:
+            messages.error(request, "Unsuccessful")
+    else:
+        print('not post?')
+        tempForm = ConfWorkForm(instance=entry)
+        context = {
+                'tempForm':tempForm,		
+                }
+        return render(request, 'edit_cred.html', context)
+
+def edit_extserv(request, pk):
+    entry = get_object_or_404(ExtensionServices, pk=pk)
+    
+    if request.method == "POST":
+        #print('post')
+        tempForm = ExtServForm(request.POST, request.FILES, instance=entry)
+        if tempForm.is_valid():
+            print('valid')       
+            instance = tempForm.save(commit=False)           
+            instance.save()
+            messages.success(request, "Changes saved successfully!")       
+            return redirect("/profile")                    
+        else:
+            messages.error(request, "Unsuccessful")
+    else:
+        print('not post?')
+        tempForm = ExtServForm(instance=entry)
+        context = {
+                'tempForm':tempForm,		
+                }
+        return render(request, 'edit_cred.html', context)
+
+def delete_educ_att(request, pk):
+    EducationalAttainment.objects.filter(pk=pk).delete()
+    return redirect('/profile')
+
+def delete_workexp(request, pk):
+    WorkExperience.objects.filter(pk=pk).delete()
+    return redirect('/profile')
+
+def delete_acc_events(request, pk):
+    AccomplishmentsEvents.objects.filter(pk=pk).delete()
+    return redirect('/profile')
+
+def delete_pub(request, pk):
+    Publications.objects.filter(pk=pk).delete()
+    return redirect('/profile')
+
+def delete_res_grants(request, pk):
+    ResearchGrants.objects.filter(pk=pk).delete()
+    return redirect('/profile')
+
+def delete_licexam(request, pk):
+    LicensureExam.objects.filter(pk=pk).delete()
+    return redirect('/profile')
+
+def delete_trainsem(request, pk):
+    TrainingSeminars.objects.filter(pk=pk).delete()
+    return redirect('/profile')
+
+def delete_confwork(request, pk):
+    ConferenceWorkshops.objects.filter(pk=pk).delete()
+    return redirect('/profile')
+
+def delete_extserv(request, pk):
+    ExtensionServices.objects.filter(pk=pk).delete()
+    return redirect('/profile')
+
+
