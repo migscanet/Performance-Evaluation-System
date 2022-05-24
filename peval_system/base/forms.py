fdd2ca9d3b28348e8acc4264a4a344941d319df4
Miguel Canet <mlcanet@up.edu.ph>
2022-05-24 16:40:49 +0800

    Edited forms

---
+++ peval_system/base/forms.py
@@ -0,0 +1,161 @@
+from django import forms
+from .models import *
+from users.models import User, FacultyProfile, UnitHeadProfile, DepartmentHeadProfile, AdminProfile
+
+class EducationalAttainmentForm(forms.ModelForm):
+    class Meta:
+        model = AddEducationalAttainment
+        fields = [
+            'institution',
+            'degree',
+            'certification',
+            'major',
+            'specialization',
+            'degree_type',
+            'start_date',
+            'end_date',
+            'proof',
+            'is_approved',
+            'is_verified',
+            'comments_remarks'
+        ]
+
+class WorkExperienceForm(forms.ModelForm):
+    class Meta:
+        model = AddWorkExperience
+        fields = [
+            'is_within',
+            'employer_name',
+            'position',
+            'start_date',
+            'end_date',
+            'description',
+            'proof',
+            'is_approved',
+            'is_verified',
+            'comments_remarks'
+        ]
+
+class AccomplishmentsEventsForm(forms.ModelForm):
+    class Meta:
+        model = AddAccomplishmentsEvents
+        fields = [
+            'accomplishment_title',
+            'type',
+            'description',
+            'start_date',
+            'end_date',
+            'date_created',
+            'image',
+            'comments_remarks',
+            'is_verified',
+            'is_approved'
+        ]
+
+class PublicationsForm(forms.ModelForm):
+    class Meta:
+        model = AddPublications
+        fields = [
+            'is_dpsm',
+            'publication_name',
+            'citation',
+            'url',
+            'date_published',
+            'is_approved',
+            'is_verified',
+            'comments_remarks',
+            'publication_type',
+            'publication_location',
+            'volume_issue_num',
+            'publication_url',
+            'publication_doi',
+            'publication_isbn',
+            'is_isi',
+            'is_elseviers_scopus',
+            'is_ched_recognized',
+            'other_collection',
+            'is_funded_up_gaa',
+            'uiob',
+            'proof_publication',
+            'proof_utilization',
+            'is_presented'
+        ]
+
+class TrainingSeminarsForm(forms.ModelForm):
+    class Meta:
+        model = AddTrainingSeminars
+        fields = [
+            'training_name',
+            'role',
+            'remarks',
+            'start_date',
+            'end_date',
+            'proof',
+            'is_approved',
+            'is_verified'
+            'comments_remarks'
+        ]
+
+class ResearchGrantsForm(forms.ModelForm):
+    class Meta:
+        model = AddResearchGrants
+        fields  = [
+            'is_dpsm',
+            'research_name',
+            'sponsor_name',
+            'grant_amount',
+            'project_start_date',
+            'project_end_date',
+            'actual_start_date',
+            'actual_end_date',
+            'research_progress',
+            'is_approved',
+            'is_verified',
+            'comments_remarks'
+        ]
+
+class LicensureExamForm(forms.ModelForm):
+    model = AddLicensureExam
+    fields = [
+        'exam_name',
+        'rank',
+        'license_number',
+        'date_exam',
+        'proof',
+        'is_approved',
+        'is_verified',
+        'comments_remarks'
+    ]
+
+class ConferenceWorkshopsForm(forms.ModelForm):
+    model = AddConferenceWorkshops
+    fields = [
+        'event_name',
+        'paper_title',
+        'presentaiton_type',
+        'conference_title',
+        'organizer_name',
+        'conference_location',
+        'conference_venue',
+        'start_date',
+        'end_date',
+        'presentation_date',
+        'fund_source',
+        'uriob',
+        'is_verified',
+        'is_approved',
+        'comments_remarks'
+    ]
+
+class FacultyServiceRecordForm(models.Model):
+    model = FacultyServiceRecord
+    fields = [
+        'course_code',
+        'section',
+        'semester',
+        'school_year',
+        'days',
+        'time',
+        'number_students',
+        'comments'
+    ]
--- peval_system/base/views.py
+++ peval_system/base/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import auth, messages
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
 
-# Create your views here.
+#Admin Functionalities
+
+# @login_required(login_url='/login',)
+def create_account():
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("User created successfully!")
+    else:
+        form = UserCreationForm()
+        return render(request, 'auth/create_user.html', {'form': form})
+
+
+# @login_required(login_url='/login',)
+def update_faculty_info():
+    print()
+
+# @login_required(login_url='/login',)
+def update_FSR():
+    print()
+
+# @login_required(login_url='/login',)
+def update_SET():
+    print()
+
