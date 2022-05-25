82d221afaca54c5100d0d95f0d4aa510b1fc452f
Aldrea Santos <aldreasantos@gmail.com>
2022-05-26 07:33:54 +0800

    unit head updates

--- peval_system/users/models.py
+++ peval_system/users/models.py
@@ -86,34 +86,41 @@ ROLECHOICES=(
     ('4', 'Admin Clerk')
 )
 
+#ADD DEPARTMENT UNIT
+#ADD MESSAGES per entity kasi need ng message hahahahah wawit for rejection hahaha
+#Check EXCEL HUHUHUH ANDAMI ILALAGAY and may ilalagay na mga bagong entity
+#Dapat meron din nag ttrack ng mga student achievements HUHUHUHU WAWITER
+# maglagay ng isApproved for DPSM head, kulang kasi kung sa unit head lang
+#add boolean for faculty if what dpsm unit siya ehehehe
 class UserManager(BaseUserManager):
-    def create_user(self, email, alternative_email, password=None):
+    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
         if not email:
             raise ValueError('Users must have an email address')
         now = timezone.now()
         email = self.normalize_email(email)
         user = self.model(
-            email=email,
-            alternative_email = alternative_email,           
+            email=email,      
+            is_staff=is_staff,
+            is_superuser=is_superuser,      
             is_active=True,            
             last_login=now,
             date_joined=now, 
+            **extra_fields
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, alternative_email, password):
-        user=self.create_user(email = email, alternative_email = alternative_email, password = password)
-        user.is_superuser = True
-        user.is_staff = True
+    def create_user(self, email, password, **extra_fields):
+        return self._create_user(email, password, False, False, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        user=self._create_user(email, password, True, True, **extra_fields)
         user.save(using=self._db)
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length=254, unique=True)
-    alternative_email = models.EmailField(max_length=254, unique=True)
-
+    #user_id = models.UUIDField(primary_key=True, unique=True)
     #Classification
     department = models.CharField(max_length=240, choices=DEPARTMENTCHOICES, default=DEPARTMENTCHOICES[0])
     faculty_rank = models.CharField(max_length=20, choices=FACULTYRANKCHOICES, default=FACULTYRANKCHOICES[0])
@@ -149,7 +156,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     #Contact Details
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     landline_number = models.CharField(max_length=20, null=True, blank=True)
-
+    email = models.EmailField(max_length=254, unique=True)
+    alternative_email = models.EmailField(max_length=254, unique=True)
 
     #Emergency Contact Details
     emergency_contact_name = models.CharField(max_length=254, null=True, blank=True)
@@ -158,10 +166,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     emergency_contact_relationship = models.CharField(max_length=20, null=True, blank=True)
 
     USERNAME_FIELD = 'email'
-
-    REQUIRED_FIELDS = [
-        'alternative_email'
-    ]
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     objects = UserManager()
 
@@ -170,3 +176,34 @@ class User(AbstractBaseUser, PermissionsMixin):
     
     def __str__(self):
         return "%s" % self.email
+    
+# class FacultyProfile(models.Model):
+#     user = models.OneToOneField(User, related_name= 'faculty_profiles', on_delete = models.CASCADE)
+#     profile_picture = models.ImageField(blank = True, upload_to = None)
+#     is_assigned = models.BooleanField(default=False)
+#     is_approved = models.BooleanField(default=False)
+
+#     def __str__(self):
+#         return f'{self.user.first_name} {self.user.last_name}'
+
+# class UnitHeadProfile(models.Model):
+#     user = models.OneToOneField(User, related_name= 'unithead_profiles', on_delete = models.CASCADE)
+#     profile_picture = models.ImageField(blank = True, upload_to = None)
+#     is_approved = models.BooleanField(default=False)
+
+#     def __str__(self):
+#         return f'{self.user.first_name} {self.user.last_name}'
+
+# class DepartmentHeadProfile(models.Model):
+#     user = models.OneToOneField(User, related_name= 'departmenthead_profiles', on_delete = models.CASCADE)
+#     profile_picture = models.ImageField(blank = True, upload_to = None)
+
+#     def __str__(self):
+#         return f'{self.user.first_name} {self.user.last_name}'
+
+# class AdminProfile(models.Model):
+#     user = models.OneToOneField(User, related_name= 'admin_profiles', on_delete = models.CASCADE)
+#     profile_picture = models.ImageField(blank = True, upload_to = None)
+
+#     def __str__(self):
+#         return f'{self.user.first_name} {self.user.last_name}'
